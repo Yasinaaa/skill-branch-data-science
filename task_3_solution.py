a6dd3bfe39dc959d6317bb909fa2f85b986b8da1
Yasina Zalyalova <zalyalovayasina@gmail.com>
2023-01-18 21:25:50 +0300

    update task

--- task_3_solution.py
+++ task_3_solution.py
@@ -1,57 +1,50 @@
 import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.base import TransformerMixin
+from sklearn.model_selection import train_test_split
 
-#1 посчитать значений производной функции $\cos(x) + 0.05x^3 + \log_2{x^2}$ в точке $x = 10$. 
-#Ответ округлить до 2-го знака. Пожалуйста назовите функцию derivation, функция должна принимать точку, в 
-#которой нужно вычислить значение производной, и функцию, производную которой мы хотим вычислить.
+def split_data_into_two_samples(x):
+    return train_test_split(x, test_size=0.3, random_state=42) 
 
-dx = 0.00001
+def prepare_data(x):
+    target_vector = x['price_doc']
+    objects = x.select_dtypes(include=['object']).columns
+    filtered = x.drop(columns=objects).drop(columns=['id']).drop(columns=['price_doc']).dropna(axis=1)
+    return [filtered, target_vector]  
 
-#$\cos(x) + 0.05x^3 + \log_2{x^2}$
-def func1(x):
-    return np.cos(x) + 0.05 * x**3 + np.log2(x**2)
+def scale_data(x, transformer):
+    return transformer.fit_transform(x)
 
-def derivation(x, func):
-    return round((float(func(x + dx) - func(x)) / dx), 2)
+def prepare_data_for_model(x, transformer):
+    x_train, y_train = prepare_data(x)
+    scaled_x = scale_data(x_train, transformer)
+    x_train_scaled = pd.DataFrame(scaled_x, columns=x_train.columns)
+    return [x_train_scaled, y_train]
 
-#x1 = 10
-#print(derivation(x1, func1))    
+def fit_first_linear_model(x_train, y_train):
+    x_train_s = scale_data(x_train, StandardScaler())
+    clr = LinearRegression()
+    clr.fit(x_train_s, y_train)
+    return clr
 
-#2 посчитать значение градиента функции $x_1^2\cos(x_2) + 0.05x_2^3 + 3x_1^3\log_2{x_2^2}$ в точке $(10, 1)$. 
-#Пожалуйста назовите функцию gradient, функция должна принимать список с координатами точки, 
-#в которой нужно вычислить значение производной, и функцию, производную которой мы хотим вычислить. Ответ округлить до 2-го знака.
-def func2(x):
-    return x[0]**2 * np.cos(x[1]) + 0.05 * x[1]**3 + 3 * x[0]**3 * np.log2(x[1]**2)   
+def fit_first_linear_model2(x_train, y_train):
+    x_train_s = scale_data(x_train, MinMaxScaler())
+    clr = LinearRegression()
+    clr.fit(x_train_s, y_train)
+    return clr
 
-def gradient(x, func):
-    return [round(float((func([x[0] + dx, x[1]]) - func(x))) / dx, 2),
-            round(float((func([x[0], x[1] + dx]) - func(x))) / dx, 2)]
+def evaluate_model(linreg, x_pred, y_true):
+    y_pred = linreg.predict(x_pred)
+    round_num = 2
+    mse = round(mean_squared_error(y_true, y_pred), round_num)
+    mae = round(mean_absolute_error(y_true, y_pred), round_num)
+    r2 = round(r2_score(y_true, y_pred), round_num)
+    return [mse, mae, r2]
 
-#twoX = [10, 1] 
-#print(gradient(twoX, func2))
-
-#3 найти точку минимуму для функции $\cos(x) + 0.05x^3 + \log_2{x^2}$.
-#Зафиксировать параметр $\epsilon = 0.001$, начальное значение принять равным 10. 
-#Выполнить 50 итераций градиентного спуска. Ответ округлить до второго знака; 
-#Пожалуйста назовите функцию gradient_optimization_one_dim. Функция должна принимать на вход функцию, которую требуется оптимизировать.
-epsilon = 0.001
-iterations = 50
-
-def gradient_optimization_one_dim(func):
-    x = 10
-    for i in range(iterations):
-        x = x - epsilon * derivation(x, func)
-    return round(x, 2)
-
-#print(gradient_optimization_one_dim(func1))
-
-#4 найти точку минимуму для функции $x_1^2\cos(x_2) + 0.05x_2^3 + 3x_1^3\log_2{x_2^2}$. 
-#Зафиксировать параметр $\epsilon = 0.001$, начальные значения весов принять равным [4, 10]. s
-#Выполнить 50 итераций градиентного спуска. Ответ округлить до второго знака; Пожалуйста назовите функцию gradient_optimization_multi_dim.
-def gradient_optimization_multi_dim(func):
-    x1 = 4
-    x2 = 10
-    for i in range(iterations):
-        funcGradient = gradient([x1, x2], func)
-        x1 = x1 - round(epsilon * funcGradient[0], 2)
-        x2 = x2 - round(epsilon * funcGradient[1], 2)
-    return [round(x1, 2), round(x2, 2)]
+def calculate_model_weights(model, features):
+    df = pd.DataFrame({'features': features, 'weights': model.coef_})
+    df.sort_values(by=['weights'])
+    return df
